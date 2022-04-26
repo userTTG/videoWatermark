47c321e7a5fcd0f4806b1c71aad3de98a800a284
张皓(10029499) <zhangh-be@glodon.com>
2022-04-26 19:44:51 +0800

    输出h264文件

--- app/src/main/java/com/zhh/zhhcamera/test/MP4Player.java
+++ app/src/main/java/com/zhh/zhhcamera/test/MP4Player.java
@@ -2,13 +2,17 @@ package com.zhh.zhhcamera.test;
 
 import android.content.Context;
 import android.media.MediaCodec;
+import android.media.MediaCodecInfo;
 import android.media.MediaExtractor;
 import android.media.MediaFormat;
+import android.os.Environment;
 import android.util.Log;
 import android.view.Surface;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @ClassName H264Player
@@ -26,10 +30,13 @@ public class MP4Player implements Runnable {
     private Surface surface;
     private MediaExtractor mediaExtractor;
     private MediaCodec mediaCodec;
+    private MediaCodec encoder;
     private MediaFormat mediaFormat;
     private Context context;
     private volatile boolean isDecodeFinish = false;
     private long timeOfFrame = 30;
+
+    private ReentrantLock mLock = new ReentrantLock();
 
     public MP4Player(Context context, String path, Surface surface) {
 
@@ -59,11 +66,25 @@ public class MP4Player implements Runnable {
 
         try {
             this.mediaCodec = MediaCodec.createDecoderByType(MediaFormat.MIMETYPE_VIDEO_AVC);
+
             // 视频宽高暂时写死
 //            MediaFormat mediaFormat = MediaFormat.createVideoFormat(MediaFormat.MIMETYPE_VIDEO_AVC, 1280, 720);
 //            mediaFormat.setInteger(MediaFormat.KEY_FRAME_RATE, 15);
             mediaCodec.configure(mediaFormat, surface, null, 0);
-        } catch (IOException e) {
+
+
+            encoder = MediaCodec.createEncoderByType(MediaFormat.MIMETYPE_VIDEO_AVC);
+            //视频信息配置
+            MediaFormat format = MediaFormat.createVideoFormat(MediaFormat.MIMETYPE_VIDEO_AVC, mediaFormat.getInteger("width"), mediaFormat.getInteger("height"));
+            //颜色
+            format.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420Flexible);
+            format.setInteger(MediaFormat.KEY_FRAME_RATE,15);//帧数
+            format.setInteger(MediaFormat.KEY_BIT_RATE,400000);//比特率
+            format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL,30);//i帧间隔
+            //设置配置信息给mediaCodec
+            encoder.configure(format,null,null,MediaCodec.CONFIGURE_FLAG_ENCODE);
+
+        } catch (Exception e) {
             // 解码芯片不支持，走软解
             e.printStackTrace();
         }
@@ -72,15 +93,17 @@ public class MP4Player implements Runnable {
 
     public void play() {
         mediaCodec.start();
+        encoder.start();
         new Thread(this::run).start();
+        new Thread(() -> encodeH264()).start();
     }
-
     @Override
     public void run() {
         // 解码 h264
         try {
             decodeMP4();
-        }catch (Exception ignore){
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
 
@@ -105,6 +128,15 @@ public class MP4Player implements Runnable {
                     int outIndex = mediaCodec.dequeueOutputBuffer(bufferInfo, 0);
                     Log.d(TAG, "outIndex: " + outIndex);
                     if (outIndex >= 0) {
+                        ByteBuffer buffer = mediaCodec.getOutputBuffer(outIndex);
+
+                        int enInputIndex = encoder.dequeueInputBuffer(0);
+                        if (enInputIndex>-1){
+                            ByteBuffer enInputBuffer = encoder.getInputBuffer(enInputIndex);
+                            enInputBuffer.clear();
+                            enInputBuffer.put(buffer);
+                            encoder.queueInputBuffer(enInputIndex, 0, enInputBuffer.limit(), 0, 0);//通知编码器 数据放入
+                        }
                         mediaCodec.releaseOutputBuffer(outIndex, true);
                     }
                     //控制帧率在30帧左右
@@ -125,15 +157,81 @@ public class MP4Player implements Runnable {
         }
     }
 
-    public void close() {
-        if (mediaCodec != null) {
-            mediaCodec.stop();
-            mediaCodec.release();
-            mediaExtractor.release();
-            isDecodeFinish = true;
-            mediaCodec = null;
-            mediaExtractor = null;
+    private void encodeH264(){
+        MediaCodec.BufferInfo bufferInfo = new MediaCodec.BufferInfo();
+        while (!isDecodeFinish){
+            mLock.lock();
+            try {
+                if (encoder != null){
+                    //获取可用ByteBuffer下标
+                    int index = encoder.dequeueOutputBuffer(bufferInfo, 100000);
+                    if(index>=0){
+                        ByteBuffer buffer = encoder.getOutputBuffer(index);
+                        byte[] outData=new byte[bufferInfo.size];
+                        //给outData设置数据
+                        buffer.get(outData);
+                        //写入文件
+                        writeContent(outData);
+                        //释放资源
+                        encoder.releaseOutputBuffer(index,false);
+                    }
+                }
+            }finally {
+                mLock.unlock();
+            }
         }
+    }
+
+    public   String writeContent(byte[] array) {
+        char[] HEX_CHAR_TABLE = {
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
+        };
+        StringBuilder sb = new StringBuilder();
+        for (byte b : array) {
+            sb.append(HEX_CHAR_TABLE[(b & 0xf0) >> 4]);
+            sb.append(HEX_CHAR_TABLE[b & 0x0f]);
+        }
+        FileWriter writer = null;
+        try {
+
+            writer = new FileWriter(Environment.getExternalStorageDirectory()+"/zhh_mp4.txt", true);
+            writer.write(sb.toString());
+            writer.write("\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(writer != null){
+                    writer.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
+
+    public void close() {
+        mLock.lock();
+        try {
+            if (mediaCodec != null) {
+                mediaCodec.stop();
+                mediaCodec.release();
+                mediaCodec = null;
+                mediaExtractor.release();
+                mediaExtractor = null;
+            }
+            if (encoder != null) {
+                encoder.stop();
+                encoder.release();
+                encoder = null;
+            }
+
+        }finally {
+            mLock.unlock();
+            isDecodeFinish = true;
+        }
+
     }
 
     public int[] getSize(){
